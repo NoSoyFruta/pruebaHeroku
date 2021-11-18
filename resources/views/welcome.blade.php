cee032a33c709cba700cd7a1b775eead373384a2
nosoyfruta <l17181079@slp.tecnm.mx>
2021-11-18 17:32:04 -0600

    Nombre en laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,9 @@
             }
         </style>
     </head>
-    <h1>Jazmin Dominguez 17181079</h1><br/>
+    
+    <h1>Jazmin Dominguez 17181079</h1>
+
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
